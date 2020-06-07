b7936bacb6ed3604f73d7ca7a8d11d39681337e4
Victor Farias <victor.f.farias@gmail.com>
2020-06-07 17:31:16 -0300

    Initial Commit

---
+++ server/src/aula2.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const users = ['Victor', 'João', 'Maria', 'Teste'];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+
+    return response.json(user)
+});
+
+app.listen(3333);
